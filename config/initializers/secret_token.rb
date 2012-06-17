25ac23b6b66ac1c680405031f1110e1e6895b84a
Julius Tarng <julius@tarng.com>
2012-06-17 08:30:52 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Savv::Application.config.secret_token = '6a4006cf5c1be57f5b0f274465bcf41af52de00a5169054970f8dfbbb0f5e5e3460aecb7bca4264145f2c8c09cd19a22cce7e73471aedff03f77ec67c66f92d7'
